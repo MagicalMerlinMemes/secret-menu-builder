7c0ab2af4d3c15b40ae752da10837f0a09bf1eda
Magical Merlin <ctrevornelson@gmail.com>
2026-09-04 13:13:48 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,20 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
-
-const rootEl = document.getElementById('root')
-const app = (
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
 )
-
-// Prerendered pages ship real markup inside #root, so hydrate instead of
-// clobbering it with a fresh client render.
-if (rootEl.hasChildNodes()) {
-  ReactDOM.hydrateRoot(rootEl, app)
-} else {
-  ReactDOM.createRoot(rootEl).render(app)
-}
